c3e8e94a7dfd81258cfd78e841e08d1b022de185
dayanacossio <70823176+yontaru@users.noreply.github.com>
2021-09-18 09:51:33 -0500

    AppPrimera

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default function App(){
+    return(
+      <View style = {styles.container}>
+        <Text style = {[styles.RojoGrande]}>Hola</Text>
+          <StatusBar style="auto" />
+          
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+        <View>
+          <Text style= {[styles.AzulGrande]}>Formatos</Text>
+        </View>
+
+        <View>
+          <Button title="Presionar" onPress={() => Alert.alert('Presionó botón') }>
+            
+          </Button>
+        </View>
+
+        <View>
+          <TextInput>
+
+          </TextInput>
+        </View>
+
+      </View>
+    );
+  }
+
+const styles = StyleSheet.create
+({
+  container:{
+    marginTop: 250,
+    flex:1,
+    padding: 50,
+    backgroundColor: "white",
   },
+
+  AzulGrande:{
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+    textAlign: "center",
+  },
+
+  RojoGrande:{
+    color: 'red',
+    textAlign: "center",
+  },
+
+  CorreoInput:{
+    underlineColorAndroid: "transparent",
+    placeholder: "Email",
+    onChangeText: this.handleEmail,
+  },
+
+  DatoNumerico:{
+
+  },
+
+  Alfanumerico:{
+
+  },
+
+  PasswordC:{
+    underlineColorAndroid: "transparent",
+    placeholder: "Clave",
+    placeholderTextColor: "blue",
+    onChangeText: this.handlePassword,
+  },
+
 });
